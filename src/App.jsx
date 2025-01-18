fbf9d6d5b4f3cfc0944f81d204f5ba167e4f812a
benny <seanhong1215@gmail.com>
2025-01-18 21:00:51 +0800

    調整 vite.config.js base url 路徑以及環境的參數設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './public/images/react.svg'
-import viteLogo from './public/images/vite.svg'
+import reactLogo from '@/assets/images/react.svg'
+import viteLogo from '@/assets/images/vite.svg'
 import './App.css'
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/taiwan-culture-project',
+  base: process.env.NODE_ENV === 'production' ? 'taiwan-culture-project' : '/',
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0, // 禁用內聯資源，確保所有文件打包到 dist
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // 設置 @ 為 src 目錄的別名
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
