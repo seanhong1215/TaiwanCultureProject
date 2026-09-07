45bc486e9b0887cbd7087168bab1c827d0c149fe
seanhong1215 <seanhong1215@gmail.com>
2026-09-07 17:40:26 +0800

    Updates

---
+++ assets/useMember-885uaZGZ.js
@@ -0,0 +1,1 @@
+import{u,b as a,c as n}from"./vendor-query-BtxTz4nf.js";import{u as m,i as o}from"./index-Bj0W7o9v.js";const y=e=>u({queryKey:["member",e],queryFn:()=>o(e),enabled:!!e}),c=e=>{const s=a();return n({mutationFn:t=>m(e,t),onSuccess:(t,r)=>{s.setQueryData(["member",e],r)}})};export{c as a,y as u};
