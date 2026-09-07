385dda72912ca6addd1061b09249860bb2efa857
seanhong1215 <seanhong1215@gmail.com>
2026-09-07 10:28:31 +0800

    Updates

---
+++ assets/notification-D3gZ0LSh.js
@@ -0,0 +1,1 @@
+import"./index-BaLK5W6H.js";import{al as a}from"./vendor-BWhtsh4R.js";const n=async()=>(await a.get("/api/notifications")).data,e=async t=>(await a.post("/api/notifications",t)).data;export{e as a,n as g};
